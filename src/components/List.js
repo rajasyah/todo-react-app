ecbd410823956678149c3a29a1f65c4767dc3651
rajasyah <rajasyahacp20@gmail.com>
2022-03-27 18:37:36 +0700

    complete fiture

--- src/components/List.js
+++ src/components/List.js
@@ -5,16 +5,40 @@ const List = ({ todos, setList }) => {
     <ul>
       {todos.map((todo) => (
         <li
-          className="flex justify-between border-2 pl-2 mb-4 text-white font-mono "
+          className="flex justify-between bg-black border-2 pl-2 mb-4 text-white font-mono "
           key={todo.id}
         >
-          {todo.completed ? "Complete" : todo.text}
-          <button
-            onClick={() => setList(todos.filter((el) => el.id !== todo.id))}
-            className="bg-black px-2"
-          >
-            x
-          </button>
+          {todo.completed ? (
+            <p className="line-through">{todo.text}</p>
+          ) : (
+            todo.text
+          )}
+          <div className="">
+            <button
+              onClick={() =>
+                setList(
+                  todos.map((item) => {
+                    if (item.id === todo.id) {
+                      return {
+                        ...item,
+                        completed: !item.completed,
+                      };
+                    }
+                    return item;
+                  })
+                )
+              }
+              className="bg-green-600 px-2"
+            >
+              &#10003;
+            </button>
+            <button
+              onClick={() => setList(todos.filter((el) => el.id !== todo.id))}
+              className="bg-red-600 px-2"
+            >
+              &#9747;
+            </button>
+          </div>
         </li>
       ))}
     </ul>
